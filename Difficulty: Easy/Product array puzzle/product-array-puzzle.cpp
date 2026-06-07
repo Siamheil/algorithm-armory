da040ef630c22a7680be08df0ca7b58d65cf7f57
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2026-06-07 10:36:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Product array puzzle/product-array-puzzle.cpp
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.cpp
@@ -1,22 +1,17 @@
-// User function template for C++
-
 class Solution {
   public:
     vector<int> productExceptSelf(vector<int>& arr) {
-        // code here
         int n=arr.size();
-        vector<int>res(n);
-        vector<int>left(n,1);
-        vector<int>right(n,1);
+        vector<int>result(n);
+        result[0]=1;
         for(int i=1;i<n;i++){
-            left[i]=left[i-1]*arr[i-1];
+            result[i]=result[i-1]*arr[i-1];
         }
-        for(int i=n-2;i>=0;i--){
-            right[i]=right[i+1]*arr[i+1];
+        int rightprod=1;
+        for(int i=n-1;i>=0;i--){
+            result[i]=result[i]*rightprod;
+            rightprod*=arr[i];
         }
-        for(int i=0;i<n;i++){
-            res[i]=left[i]*right[i];
-        }
-        return res;
+        return result;
     }
 };
