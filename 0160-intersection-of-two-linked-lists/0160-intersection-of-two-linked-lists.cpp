0f3ce6499a64173095a4d6b189f2dd2187607e2a
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2026-07-19 16:32:52 +0530

    Time: 58 ms (47.96%), Space: 23.8 MB (86.88%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -1,39 +1,29 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *head1, ListNode *head2) {
-        ListNode *curr1=head1,*curr2=head2;
-        int count1=0,count2=0;
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int cnt1=0,cnt2=0;
+        ListNode* curr1=headA,*curr2=headB;
         while(curr1){
-            count1++;
+            cnt1++;
             curr1=curr1->next;
         }
         while(curr2){
-            count2++;
+            cnt2++;
             curr2=curr2->next;
         }
-        curr1=head1;
-        curr2=head2;
-        while(count1>count2){
+        curr1=headA,curr2=headB;
+        while(cnt1>cnt2){
+            cnt1--;
             curr1=curr1->next;
-            count1--;
         }
-        while(count2>count1){
+        while(cnt2>cnt1){
+            cnt2--;
             curr2=curr2->next;
-            count2--;
         }
         while(curr1!=curr2){
             curr1=curr1->next;
             curr2=curr2->next;
         }
         return curr1;
-        if(!curr1) return NULL;
     }
 };
