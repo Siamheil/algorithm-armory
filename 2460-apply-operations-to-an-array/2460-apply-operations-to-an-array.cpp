f3869652c73f74e3e13da153ed29d927a5c5ea56
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2026-08-05 19:44:20 +0530

    Time: 0 ms (100%), Space: 13.3 MB (27.01%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
-    void movezeroestoend(vector<int>& nums){
+    vector<int> applyOperations(vector<int>& nums) {
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]){
+                nums[i]=nums[i]*2;
+                nums[i+1]=0;
+            }
+        }
         int j=-1;
-
         for(int i=0;i<nums.size();i++){
             if(nums[i]==0){
                 j=i;
                 break;
             }
         }
-
-        if(j==-1) return;
-
+        if(j==-1) return nums;
         for(int i=j+1;i<nums.size();i++){
             if(nums[i]!=0){
                 swap(nums[i],nums[j]);
                 j++;
             }
         }
-    }
-
-    vector<int> applyOperations(vector<int>& nums) {
-        int n=nums.size();
-
-        for(int i=0;i<n-1;i++){
-            if(nums[i]==nums[i+1] && nums[i]!=0){
-                nums[i]*=2;
-                nums[i+1]=0;
-            }
-        }
-
-        movezeroestoend(nums);
         return nums;
     }
 };
