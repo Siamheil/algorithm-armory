038eaa4eee2e2f4da220a02b008377791e4c23e9
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2026-08-07 20:54:34 +0530

    Time: 0 ms (100%), Space: 9 MB (8.92%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int length = 0;
-        bool counting = false;
-        
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (s[i] != ' ') {
-                counting = true;
-                length++;
-            }
-            else if (counting) {
-                break;
-            }
+        int n=s.size();
+        int length=0;
+        int i=n-1;
+        while(i>=0 && s[i]==' ') i--;
+        while(i>=0 && s[i]!=' '){
+            length++;
+            i--;
         }
-        
         return length;
     }
 };
