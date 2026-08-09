d716006c2e178c65c07efcb27de2633ca962dd4a
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2026-08-09 12:09:17 +0530

    Time: 0 ms (100%), Space: 11.5 MB (62.71%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -3,15 +3,16 @@ public:
     vector<int> plusOne(vector<int>& digits) {
         int n=digits.size();
         for(int i=n-1;i>=0;i--){
-            if(digits[i]<9){
-                digits[i]++;
+            if(digits[i]+1!=10){
+                digits[i]=digits[i]+1;
                 return digits;
             }
-            else{
-                digits[i]=0;
+            digits[i]=0;
+            if(i==0){
+                digits.insert(digits.begin(),1);
+                return digits;
             }
         }
-        digits.insert(digits.begin(),1);
         return digits;
     }
 };
