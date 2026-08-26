27a5b2d6378c4229e96aebb56c7f9abf9b2daa48
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2026-08-26 18:09:44 +0530

    Time: 6 ms (27.5%), Space: 15.3 MB (92.85%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
+    static auto compare(int a,int b){
+        string sa=to_string(a);
+        string sb=to_string(b);
+        if((sa+sb)>(sb+sa)) return true;
+        return false;
+    }
     string largestNumber(vector<int>& nums) {
-        auto myComparator=[](int &a,int &b){
-            string s1=to_string(a);
-            string s2=to_string(b);
-            return s1+s2>s2+s1;
-        };
-        sort(nums.begin(),nums.end(),myComparator);
-        if(nums[0]==0) return "0";
-        string result="";
-        for(auto x:nums){
-            result+=to_string(x);
+        string ans="";
+        sort(nums.begin(),nums.end(),compare);
+        for(auto i:nums){
+            string s=to_string(i);
+            ans+=s;
         }
-        return result;
+        if(ans[0] == '0')
+            return "0";
+        return ans; 
     }
 };
