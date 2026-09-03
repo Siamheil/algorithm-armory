7f3705e86c9ab55db9b4d871627347f0c10e403f
Harshit Vishwakarma <harshitvishwa826@gmail.com>
2026-09-03 19:03:19 +0530

    Time: 5 ms (43.47%), Space: 33.6 MB (5.45%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
@@ -1,45 +1,20 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    TreeNode* construct(vector<int>&arr,int l,int r){
-        if(l>r) return NULL;
-        int mid=l+(r-l)/2;
+    TreeNode* solve(vector<int>& arr,int start,int end){
+        if(start>end) return nullptr;
+        int mid=start+(end-start)/2;
         TreeNode* root=new TreeNode(arr[mid]);
-        root->left=construct(arr,l,mid-1);
-        root->right=construct(arr,mid+1,r);
+        root->left=solve(arr,start,mid-1);
+        root->right=solve(arr,mid+1,end);
         return root;
-    }
-    void LLtoArray(ListNode* head,vector<int>&arr){
-        ListNode* temp=head;
-        while(temp){
-            arr.push_back(temp->val);
-            temp=temp->next;
-        }
     }
     TreeNode* sortedListToBST(ListNode* head) {
         vector<int>arr;
-        LLtoArray(head,arr);
-        int l=0,r=arr.size()-1;
-        return construct(arr,l,r);
+        ListNode* curr=head;
+        while(curr){
+            arr.push_back(curr->val);
+            curr=curr->next;
+        }
+        return solve(arr,0,arr.size()-1);
     }
 };
